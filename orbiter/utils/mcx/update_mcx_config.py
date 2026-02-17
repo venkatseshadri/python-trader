9eab301b658f8f0c8f1353921d1589f1b3d72084
Venkat Seshadri <vseshadri@manh.com>
2026-02-17 23:49:12 +0530

    fix: use correct BrokerClient import in MCX utility

--- orbiter/utils/mcx/update_mcx_config.py
+++ orbiter/utils/mcx/update_mcx_config.py
@@ -11,7 +11,7 @@ import time
 # Ensure we can import from project root (orbiter/)
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
-from core.client import BrokerClient
+from core.broker import BrokerClient
 import config.config as config
 import logging
 
