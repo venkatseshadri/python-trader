255811feebcc56765bf0b87066d4d1c00904fd1c
Venkat Seshadri <vseshadri@manh.com>
2026-02-18 19:39:26 +0530

    feat: finalized MCX universe with 15 verified tokens v3.8.5

--- orbiter/config/mcx/config.py
+++ orbiter/config/mcx/config.py
@@ -2,22 +2,20 @@ import json
 import os
 from datetime import time as dt_time
 
-# Common MCX symbols
+# Common MCX symbols including Minis and Micros (Corrected via API Discovery)
 SYMBOLS_UNIVERSE = [
-    'CRUDEOIL', 'NATURALGAS', 'GOLD', 'SILVER', 'COPPER', 'ZINC', 'LEAD', 'ALUMINIUM'
+    'CRUDEOIL', 'CRUDEOILM', 'NATURALGAS', 
+    'GOLD', 'GOLDM', 'GOLDPETAL', 
+    'SILVER', 'SILVERM', 'SILVERMIC',
+    'COPPER', 'ZINC', 'ZINCMINI', 'LEAD', 'LEADMINI', 'ALUMINIUM'
 ]
 
 # This will be populated by update_mcx_config.py
 SYMBOLS_FUTURE_UNIVERSE = [
-    'MCX|467013',  # CRUDEOIL
-    'MCX|467385',  # NATURALGAS
-    'MCX|454818',  # GOLD
-    'MCX|451666',  # SILVER
-    'MCX|477167',  # COPPER
-    'MCX|477171',  # ZINC
-    'MCX|477168',  # LEAD
-    'MCX|477166',  # ALUMINIUM
-] 
+    'MCX|467013', 'MCX|467014', 'MCX|467385', 'MCX|454818', 'MCX|472781',
+    'MCX|472788', 'MCX|451666', 'MCX|451669', 'MCX|458305', 'MCX|477167',
+    'MCX|477171', 'MCX|477172', 'MCX|477168', 'MCX|477169', 'MCX|477166'
+]
 
 MARKET_OPEN = dt_time(9, 0)
 MARKET_CLOSE = dt_time(23, 30)
